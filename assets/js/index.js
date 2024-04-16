c1c4ab7a40b86bf21fb7e2f6021002a3e0412c46
Jhon Alessandro <astralgmd@gmail.com>
2024-04-15 20:22:56 -0400

    feat(error): handle the functionality of error message displaying

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,10 @@
 let rating = 0;
 
 document.addEventListener("DOMContentLoaded", () => {
-  const ratingButtons = document.querySelectorAll(".rating__button");
   const submit = document.querySelector(".submit");
   const ratingCardLayout = document.querySelector(".rating-card__layout");
   const ratingCardGratitude = document.querySelector(".rating-card__gratitude");
+  const ratingButtons = document.querySelectorAll(".rating__button");
 
   /* After clicking an option it'll be selected */
   ratingButtons.forEach((button) => {
@@ -16,16 +16,68 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   /* After clicking the submit button the rating message will be displayed */
-  submitRating(submit, ratingCardLayout, ratingCardGratitude);
+
+  submitRating(submit, ratingCardLayout, ratingCardGratitude, ratingButtons);
 });
 
 /* This function will display the rating message after clicking the submit button */
-const submitRating = (submitEvent, ratingCardLayout, ratingCardGratitude) => {
+const submitRating = (
+  submitEvent,
+  ratingCardLayout,
+  ratingCardGratitude,
+  buttons
+) => {
   const ratingResult = document.querySelector(".rating__result");
+  const ratingButtons = buttons;
 
   submitEvent.addEventListener("click", () => {
-    ratingCardLayout.setAttribute("id", "hidden");
-    ratingCardGratitude.removeAttribute("id");
-    ratingResult.textContent = rating;
+    const error = document.querySelector(".error");
+
+    if (rating === 0) {
+      error.innerHTML = "Please select a rating";
+      error.style.display = "inline-block";
+      highlightRating(buttons);
+
+      setTimeout(() => {
+        error.style.opacity = "1";
+      }, 100);
+
+      // Hides the error after 3 seconds
+      setTimeout(() => {
+        error.style.opacity = "0";
+      }, 3000);
+
+      return false;
+    } else {
+      ratingCardLayout.setAttribute("id", "hidden");
+      ratingCardGratitude.removeAttribute("id");
+      ratingResult.textContent = rating;
+
+      //? In case of a new rating it'll reload the page
+      /* setTimeout(() => {
+        window.location.reload();
+      }, 5000); */
+    }
+
+    if (rating !== 0) {
+      error.style.display = "none";
+    }
   });
+};
+
+/* This functin will highlight the rating buttons */
+const highlightRating = (ratingButtons) => {
+  const highlightColor = "var(--slate-blue-darker)";
+
+  ratingButtons.forEach((button) => {
+    button.style.backgroundColor = highlightColor;
+    button.style.color = "var(--orange)";
+
+    setTimeout(() => {
+      button.style.backgroundColor = "";
+      button.style.color = "";
+    }, 300);
+  });
+
+  console.log(ratingButtons);
 };
